32c6a63efbaf677f5ee36f7c1eb2e951142e9e52
xdrown <40439836+xdrown@users.noreply.github.com>
2018-06-22 04:09:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -364,7 +364,7 @@ if (message.content.startsWith("XDcv")) {
 
 
 
-var prefix = "XD";
+var prefix = "3atia";
 
 client.on('message', message => {
   if (message.author.bot) return;
@@ -460,7 +460,7 @@ client.on('message', message => {
     }
 });
 
-  var prefix = "XD";
+  var prefix = "3atia";
 var cats = ["https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg","http://www.dogbazar.org/wp-content/uploads/2014/09/british-bull-dog-puppies.jpg","http://cdn2-www.dogtime.com/assets/uploads/gallery/german-shepherd-dog-breed-pictures/standing-7.jpg","http://cdn.akc.org/Marketplace/Breeds/German_Shepherd_Dog_SERP.jpg","https://animalso.com/wp-content/uploads/2016/12/black-german-shepherd_2.jpg","https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpg","https://www.petfinder.com/wp-content/uploads/2012/11/101438745-cat-conjunctivitis-causes.jpg","http://www.i-love-cats.com/images/2015/04/12/cat-wallpaper-38.jpg","https://www.aspca.org/sites/default/files/cat-care_urine-marking_main-image.jpg","https://s-media-cache-ak0.pinimg.com/originals/f0/3b/76/f03b7614dfadbbe4c2e8f88b69d12e04.jpg","http://www.rd.com/wp-content/uploads/sites/2/2016/04/15-cat-wants-to-tell-you-attention.jpg","https://www.thelocal.de/userdata/images/article/fa6fd5014ccbd8f4392f716473ab6ff354f871505d9128820bbb0461cce1d645.jpg","https://www.adelaidezoo.com.au/wp-content/uploads/sites/2/animals/GiantPanda3Slider.jpg","http://imagem.band.com.br/f_230168.jpg"]
     client.on('message', message => {
         var args = message.content.split(" ").slice(1);
@@ -472,7 +472,7 @@ message.channel.sendEmbed(cat);
 });
 
 client.on('message', message => {
-var prefix = "XD";
+var prefix = "3atia";
 
     if (message.author.id === client.user.id) return;
     if (message.guild) {
@@ -541,7 +541,7 @@ client.on('message', message => {
     }
 });
 
-  var prefix = "XD";
+  var prefix = "3atia";
   const HeRo = new Discord.Client();
   client.on('message', message => {
       if (message.content === prefix + "date") {
@@ -634,7 +634,7 @@ client.on('ready', () => {
 }); 
 
 client.on("message", message => {
-    var prefix = "XD";
+    var prefix = "3atia";
             var args = message.content.substring(prefix.length).split(" ");
             if (message.content.startsWith(prefix + "clear")) {
  if (!args[1]) {
@@ -1078,7 +1078,7 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-var prefix = "XD";
+var prefix = "3atia";
 
 if (!message.content.startsWith(prefix)) return;
 var args = message.content.split(' ').slice(1);
